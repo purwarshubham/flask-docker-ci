dc9345e2e9f2bf4b47be7f7750e735d35837b7ab
shubham.purwar <purwarshubham@gmail.com>
2025-06-22 14:38:03 +0530

    workflow to run pytest

---
+++ tests/basic_test.py
@@ -0,0 +1,16 @@
+def test_status_true():
+    assert True is True
+
+def test_status_false():
+    assert not False
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6
+
+def test_string_contains():
+    greeting = "hello docker world"
+    assert "docker" in greeting
+
+def test_dict_lookup():
+    user = {"id": 1, "name": "Shubham"}
+    assert user["name"] == "Shubham"
