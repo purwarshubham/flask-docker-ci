de8f7aa0fc172d790c1df3fa95ec19cd20853252
shubham.purwar <purwarshubham@gmail.com>
2025-06-22 15:21:22 +0530

    workflow to run pytest

--- tests/app_validation.py
+++ tests/app_validation.py
@@ -1,7 +1,7 @@
 from app import app
 
 def test_home_route():
-    with app.home() as client:
+    with app.test_client() as client:
         response = client.get('/')
         assert response.status_code == 200
         assert b"Hello from Docker build test!" in response.data
